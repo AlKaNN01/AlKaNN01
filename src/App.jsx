4ee24e53ef7dff14560e74726c1007c8f3786f44
ufuktan alkan <ufuktanalkan255@gmail.com>
2025-07-31 22:09:11 +0300

    react-state durumunu anlama

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { sculptureList } from "./data";
+export default App;
+
+function App() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  let sculpture = sculptureList[index];
+
+  function handlePreviousClick() {
+    if (index > 0) {
+      setIndex(index - 1);
+    } else {
+      setIndex(sculptureList.length - 1);
+    }
+  }
+
+  function handleNextClick() {
+    if (index < sculptureList.length - 1) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={handlePreviousClick}>Geri</button>
+      <button onClick={handleNextClick}>İleri</button>
+
+      <h2>
+        <i>
+          {sculpture.name} by {sculpture.artist}
+        </i>
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>
+        <button>
+          <span onClick={() => setShowMore(!showMore)}>
+            {showMore ? "Show Less" : "Show More"}
+          </span>
+        </button>
+      </p>
+      {showMore && <p>{sculpture.description}</p>}
+    </>
+  );
+}
